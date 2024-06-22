280356d349d0aa88355b070a29400c905148e26d
aibidapo <24958100+aibidapo@users.noreply.github.com>
2024-06-22 16:08:21 -0500

    db_refresh script

--- jobs/pp/db_refreshJobs.groovy
+++
@@ -1,9 +0,0 @@
-job("db_refresh-job") {
-	description()
-	keepDependencies(false)
-	disabled(false)
-	concurrentBuild(false)
-	steps {
-		shell("echo \"Hello, I am your slave system\" >> /home/ubuntu/maven.txt")
-	}
-}
